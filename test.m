7fa6b9af5c9872f6eadd36abfd6a5b9abad5f130
Alexander Liao <alexander_liao@outlook.com>
2018-05-27 21:19:19 -0400

    added script for run time evaluation

--- test.m
+++ test.m
@@ -1,3 +1,16 @@
-a=rand(100000,3);
-%e=eul2r(a);
-f=ceul2r(gpuArray(a));
+h=21;
+for i=1:h
+    a=randn(2^i,3);i
+    b=gpuArray(a);
+    tic
+    z=eul2r(a);
+    t(i)=toc;
+    
+    tic
+    z=ceul2r(b);
+    g(i)=toc;
+end
+plot(1:h,t)
+hold on
+plot(1:h,g)
+set(gca, 'YScale', 'log')
